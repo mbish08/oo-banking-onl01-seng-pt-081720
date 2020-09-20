3743920ecf284bab5d60fe289eb156900597942f
Mikel Bishop <mdrew8@gmail.com>
2020-09-20 18:23:24 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,8 +13,26 @@ class Transfer
   end 
   
   def valid?
-    sender.status == "open" && receiver.status == true
+    @sender.valid? && @receiver.valid?
   end 
   
+  def execute_transaction
+    if valid? && @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else 
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end 
+  end 
+  
+  def reverse_transfer
+    if @status == "complete"
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+      @status = "reversed"
+    end 
+  end 
   
 end
