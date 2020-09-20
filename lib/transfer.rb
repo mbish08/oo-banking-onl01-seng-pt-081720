c76c499c813ab60937f69e196d3bab5fbf0e340f
Mikel Bishop <mdrew8@gmail.com>
2020-09-20 18:23:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,10 @@ class Transfer
   end 
   
   def reverse_transfer
-    
+    if @status == "complete"
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+      @status = "reversed"
   end 
   
 end
